2f0a89080856b564a06d2fb8ac8584b029295876
NamCaoDev <namcaodev@gmail.com>
2025-03-20 09:31:26 +0700

    chore: add example examples/generated

---
+++ examples/generated/newman:ge-trequest/query.ts
@@ -0,0 +1,40 @@
+import querystring from 'query-string';
+import { NewmanGETrequestQueryParams } from "./apiQueries";
+import customFetch from "../../../helpers/fetcher";
+
+type QueryParamsCompatible<T> = {
+  [K in keyof T]: string | number | boolean | readonly string[] | readonly number[] | readonly boolean[] | null;
+};
+
+function convertQueryParams<T extends object>(params: T): QueryParamsCompatible<T> {
+  return Object.fromEntries(
+    Object.entries(params).map(([key, value]) => [
+      key,
+      value instanceof Date ? value.toISOString() : value,
+    ])
+  ) as QueryParamsCompatible<T>;
+}
+
+
+
+export const newmanGeTrequestQueryOptions = (params: NewmanGETrequestQueryParams) => ({
+  queryKey: ['Newman:GETrequest', params],
+  queryFn: async () => {
+    const queryString = querystring.stringify(convertQueryParams(params));
+
+    const res = await customFetch({
+      url: `/postman-echo.com/get` + (queryString ? `?${queryString}` : ''),
+      method: 'GET' 
+    });
+
+    const resWithParse = 
+    res;
+
+    return resWithParse;
+  },
+});
+
+export const newmanGeTrequestQueryKeys = (params: NewmanGETrequestQueryParams) => {
+  return ['Newman:GETrequest', params];
+} 
+
---
+++ examples/generated/newman:pos-trequest/mutation.ts
@@ -0,0 +1,20 @@
+import { MutationOptions } from "@tanstack/react-query";
+import customFetch from "../../../helpers/fetcher";
+
+export const newmanPosTrequestMutationOptions = (): MutationOptions<
+  null , 
+  unknown,
+  
+> => ({
+  mutationFn: async (data) => {
+    const res = await customFetch<null, >({
+      url: '',
+      method: 'POST',
+      body: data, 
+    });
+
+    const resWithParse =     res
+
+    return resWithParse;
+  },
+});
