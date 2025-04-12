3999325ebad1707a4393620bdf1af80af2a086ed
NamCaoDev <namcaodev@gmail.com>
2025-04-12 11:12:55 +0700

    fix: test push follow version

--- release.config.js
+++ release.config.js
@@ -1,10 +1,30 @@
 module.exports = {
     branches: ['main'],
     plugins: [
-      '@semantic-release/commit-analyzer',  // analyzer commit message
+      // [
+      //   "@semantic-release/commit-analyzer",
+      //   {
+      //     "preset": "conventionalcommits",
+      //     "releaseRules": [
+      //       { "type": "feat", "release": "minor" },
+      //       { "type": "fix", "release": "patch" },
+      //       { "type": "BREAKING CHANGE", "release": "major" },
+      //       { "scope": "docs", "release": "patch" }
+      //     ],
+      //     "parserOpts": {
+      //       "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES"]
+      //     }
+      //   }
+      // ], // analyzer follow tag PA
       '@semantic-release/release-notes-generator', // create changelog
       '@semantic-release/changelog', // insert CHANGELOG.md
       '@semantic-release/npm', // publish to NPM
       '@semantic-release/git', // commit again file package.json & changelog
+      [
+        "@semantic-release/git",
+        {
+          "assets": ["package.json", "package-lock.json", "CHANGELOG.md"]
+        }
+      ]
     ],
   };
